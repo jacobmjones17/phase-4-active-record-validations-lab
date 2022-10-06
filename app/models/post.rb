ffea776e05b1853680bf27135bbffa0a301c89ef
Jacob <jacobmjones17@gmail.com>
2022-10-05 20:00:09 -0400

    commit tree 31ead2c207237b6a4012de5cb9f5c6d3dd941954

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence:true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate:clickbait
+
+    def clickbait
+        unless title.inclusion ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    end 
+    
+end 
